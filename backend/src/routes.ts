5697348122c5c40c8bd99abde93de94ab5ce01c2
Erick <erickalexandrex@gmail.com>
2021-10-23 14:07:25 -0300

    create message method

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -1,8 +1,14 @@
 import { Router } from 'express';
 import { AuthenticateUserController } from './controllers/AuthenticateUserController';
+import { CreateMessageController } from './controllers/CreateMessageController';
+import { ensureAuthenticated } from './middleware/ensureAuthenticated';
 
 const router = Router();
 
+// Authenticate User
 router.post('/authenticate', new AuthenticateUserController().handle);
+
+// Send messages
+router.post('/messages', ensureAuthenticated, new CreateMessageController().handle);
 
 export { router };
